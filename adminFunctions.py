b01059de24b48164ff655cb918d2007d4e56a17d
akshat <akshatgoyalak23@gmail.com>
2019-11-12 05:54:55 +0530

    admin functions added

--- adminFunctions.py
+++ adminFunctions.py
@@ -412,3 +412,434 @@ def passengerShip(con, cur):
 		else:
 			print("Please choose the correct option")
 
+
+def agent(con, cur):
+	while(True):
+		print("Do you want to:")
+		print("1. See all entries")
+		print("2. Add new entry")
+		print("3. Modify the entry")
+		print("4. Delete the entry")
+		print("5. Go back")
+		ch = int(input("Enter choice> "))
+		if ch == 1:
+			try:
+				query = 'SELECT * FROM BookingAgent;'
+				cur.execute(query)
+				rows = cur.fetchall()
+				for row in rows:
+					print(row)
+				print("")
+			except Exception as er:
+				print("Failed to show entries")
+				print(">>>>>>>>>", er)
+		elif ch == 2:
+			try:
+				info = {}
+				info["ID"] = int(input("Agent_ID: "))
+				info["Name"] = input("Agent Name : ")
+				query = "INSERT INTO BookingAgent(Agent_ID, AgentName) VALUES('%d', '%s')" %(info["ID"], info["Name"]);
+				cur.execute(query)
+				con.commit()
+				print("Successfully Added\n")
+			except Exception as er:
+				con.rollback()
+				print("Failed to insert into database")
+				print(">>>>>>>>>", er)
+		elif ch == 3:
+			try:
+				info = {}
+				ID = int(input("Enter Agent_ID to modify: "))
+				query = "SELECT * FROM BookingAgent where Agent_ID = '%d'" %(ID);
+				cur.execute(query)
+				row = cur.fetchone()
+				print("Enter the entries you want to modify otherwise press 'ENTER'")
+				info["ID"] = input("Agent_ID: ")
+				if info["ID"] != "":
+					row["Agent_ID"] = int(info["ID"])
+				info["Name"] = input("Agent Name : ")	
+				if info["Name"] != "":
+					row["AgentName"] = info["Name"]
+				query = "UPDATE BookingAgent SET Agent_ID = '%d', AgentName = '%s' WHERE Agent_ID = '%d'" %(row["Agent_ID"], row["AgentName"], ID);
+				cur.execute(query)
+				con.commit()
+				print("Successfully Modified\n")
+			except Exception as er:
+				con.rollback()
+				print("Failed to modify in the database")
+				print(">>>>>>>>>", er)
+		elif ch == 4:
+			try:
+				info = {}
+				info["ID"] = int(input("Enter the Agent_ID you want to delete: "))
+				query = "DELETE FROM BookingAgent WHERE Agent_ID = '%d'" %(info["ID"]);
+				cur.execute(query)
+				con.commit()
+				print("Successfully Deleted\n")
+			except Exception as er:
+				con.rollback()
+				print("Failed to delete in the database")
+				print(">>>>>>>>>", er)
+		elif ch == 5:
+			return 1
+		else:
+			print("Please choose the correct option")
+
+
+def route(con, cur):
+	while(True):
+		print("Do you want to:")
+		print("1. See all entries")
+		print("2. Add new entry")
+		print("3. Modify the entry")
+		print("4. Delete the entry")
+		print("5. Go back")
+		ch = int(input("Enter choice> "))
+		if ch == 1:
+			try:
+				query = 'SELECT * FROM Route;'
+				cur.execute(query)
+				rows = cur.fetchall()
+				for row in rows:
+					print(row)
+				print("")
+			except Exception as er:
+				print("Failed to show entries")
+				print(">>>>>>>>>", er)
+		elif ch == 2:
+			try:
+				info = {}
+				info["ID"] = int(input("Ship_ID: "))
+				info["PID"] = int(input("Port_ID: "))
+				info["SNo"] = int(input("Stop Number : "))
+				info["ATime"] = input("Arrival Time (YYYY-MM-DD HH-MM-SS) : ")	
+				info["DTime"] = input("Departure Time (YYYY-MM-DD HH-MM-SS) : ")	
+				query = "INSERT INTO Route(Ship_ID, Port_ID, StopNumber, ArrivalTime, DepartureTime) VALUES('%d', '%d', '%d', '%s', '%s')" %(info["ID"], info["PID"], info["SNo"], info["ATime"], info["DTime"]);
+				cur.execute(query)
+				con.commit()
+				print("Successfully Added\n")
+			except Exception as er:
+				con.rollback()
+				print("Failed to insert into database")
+				print(">>>>>>>>>", er)
+		elif ch == 3:
+			try:
+				info = {}
+				ID = int(input("Enter Ship_ID to modify: "))
+				Time = input("Enter Departure Time (YYYY-MM-DD HH-MM-SS) to modify: ")	
+				query = "SELECT * FROM Route where Ship_ID = '%d' AND DepartureTime = '%s'" %(ID, Time);
+				cur.execute(query)
+				row = cur.fetchone()
+				print("Enter the entries you want to modify otherwise press 'ENTER'")
+				info["ID"] = input("Ship_ID: ")
+				if info["ID"] != "":
+					row["Ship_ID"] = int(info["ID"])
+				info["PID"] = input("Port_ID: ")
+				if info["PID"] != "":
+					row["Port_ID"] = int(info["PID"])
+				info["SNo"] = input("Stop Number: ")
+				if info["SNo"] != "":
+					row["StopNumber"] = int(info["SNo"])
+				info["ATime"] = input("Arrival Time (YYYY-MM-DD HH-MM-SS) : ")	
+				if info["ATime"] != "":
+					row["ArrivalTime"] = info["ATime"]
+				info["DTime"] = input("Departure Time (YYYY-MM-DD HH-MM-SS) : ")	
+				if info["DTime"] != "":
+					row["DepartureTime"] = info["DTime"]
+				query = "UPDATE Route SET Ship_ID = '%d', Port_ID = '%d', StopNumber = '%d', ArrivalTime = '%s', DepartureTime = '%s' WHERE Ship_ID = '%d' AND DepartureTime = '%s'" %(row["Ship_ID"], row["Port_ID"], row["Stop Number"], row["ArrivalTime"], row["DepartureTime"], ID, Time);
+				cur.execute(query)
+				con.commit()
+				print("Successfully Modified\n")
+			except Exception as er:
+				con.rollback()
+				print("Failed to modify in the database")
+				print(">>>>>>>>>", er)
+		elif ch == 4:
+			try:
+				info = {}
+				info["ID"] = int(input("Enter the Ship_ID you want to delete: "))
+				info["Time"] = input("Enter the Departure Time (YYYY-MM-DD HH-MM-SS) you want to delete: ")
+				query = "DELETE FROM Route WHERE Ship_ID = '%d' AND DepartureTime = '%s'" %(info["ID"], info["Time"]);
+				cur.execute(query)
+				con.commit()
+				print("Successfully Deleted\n")
+			except Exception as er:
+				con.rollback()
+				print("Failed to delete in the database")
+				print(">>>>>>>>>", er)
+		elif ch == 5:
+			return 1
+		else:
+			print("Please choose the correct option")
+
+
+def ticket(con, cur):
+	while(True):
+		print("Do you want to:")
+		print("1. See all entries")
+		print("2. Add new entry")
+		print("3. Modify the entry")
+		print("4. Delete the entry")
+		print("5. Go back")
+		ch = int(input("Enter choice> "))
+		if ch == 1:
+			try:
+				query = 'SELECT * FROM Ticket;'
+				cur.execute(query)
+				rows = cur.fetchall()
+				for row in rows:
+					print(row)
+				print("")
+			except Exception as er:
+				print("Failed to show entries")
+				print(">>>>>>>>>", er)
+		elif ch == 2:
+			try:
+				info = {}
+				info["ID"] = int(input("Ticket_ID: "))
+				info["SID"] = int(input("Ship_ID: "))
+				info["PID"] = int(input("Port_ID: "))
+				info["PsID"] = int(input("Passenger_ID: "))
+				info["AID"] = int(input("Agent_ID: "))
+				info["seat"] = int(input("Seat Number : "))
+				info["time"] = input("Departure Time (YYYY-M-DD HH-MM-SS) : ")
+				query = "INSERT INTO Ticket(Ticket_ID, Ship_ID, Port_ID, Passenger_ID, Agent_ID, SeatNo, DepartureTime) VALUES('%d', '%d', '%d', '%d', '%d', '%d', '%s')" %(info["ID"], info["SID"], info["PID"], info["PsID"], info["AID"], info["seat"], info["time"]);
+				cur.execute(query)
+				con.commit()
+				print("Successfully Added\n")
+			except Exception as er:
+				con.rollback()
+				print("Failed to insert into database")
+				print(">>>>>>>>>", er)
+		elif ch == 3:
+			try:
+				info = {}
+				ID = int(input("Enter Ticket_ID to modify: "))
+				query = "SELECT * FROM Ticket where Ticket_ID = '%d'" %(ID);
+				cur.execute(query)
+				row = cur.fetchone()
+				print("Enter the entries you want to modify otherwise press 'ENTER'")
+				info["ID"] = input("Ticket_ID: ")
+                if info["ID"] != "":
+                    row["Ticket_ID"] = int(info["ID"])
+				info["SID"] = input("Ship_ID: ")
+                if info["SID"] != "":
+                    row["Ship_ID"] = int(info["SID"])
+				info["PID"] = input("Port_ID: ")
+                if info["PID"] != "":
+                    row["Port_ID"] = int(info["PID"])
+				info["PsID"] = input("Passenger_ID: ")
+                if info["PsID"] != "":
+                    row["Passenger_ID"] = int(info["PsID"])
+				info["AID"] = input("Agent_ID: ")
+                if info["AID"] != "":
+                    row["Agent_ID"] = int(info["AID"])
+				info["seat"] = input("Seat Number : ")
+                if info["seat"] != "":
+                    row["SeatNo"] = int(info["seat"])
+				info["time"] = input("Departure Time (YYYY-M-DD HH-MM-SS) : ")
+                if info["time"] != "":
+                    row["DepartureTime"] = int(info["time"])
+				query = "UPDATE Ticket SET Ticket_ID = '%d', Ship_ID = '%d', Port_ID = '%d', Passenger_ID = '%d', Agent_ID = '%d', SeatNo = '%d', DepartureTime = '%s' WHERE Ticket_ID = '%d'" %(row["Ticket_ID"], row["Ship_ID"], row["Port_ID"], row["Passenger_ID"], row["Agent_ID"], row["SeatNo"], row["DepartureTime"], ID);
+				cur.execute(query)
+				con.commit()
+				print("Successfully Modified\n")
+			except Exception as er:
+				con.rollback()
+				print("Failed to modify in the database")
+				print(">>>>>>>>>", er)
+		elif ch == 4:
+			try:
+				info = {}
+				info["ID"] = int(input("Enter the Ticket_ID you want to delete: "))
+				query = "DELETE FROM Ticket WHERE Ticket_ID = '%d'" %(info["ID"]);
+				cur.execute(query)
+				con.commit()
+				print("Successfully Deleted\n")
+			except Exception as er:
+				con.rollback()
+				print("Failed to delete in the database")
+				print(">>>>>>>>>", er)
+		elif ch == 5:
+			return 1
+		else:
+			print("Please choose the correct option")
+
+
+def department(con, cur):
+	while(True):
+		print("Do you want to:")
+		print("1. See all entries")
+		print("2. Add new entry")
+		print("3. Modify the entry")
+		print("4. Delete the entry")
+		print("5. Go back")
+		ch = int(input("Enter choice> "))
+		if ch == 1:
+			try:
+				query = 'SELECT * FROM Department;'
+				cur.execute(query)
+				rows = cur.fetchall()
+				for row in rows:
+					print(row)
+				print("")
+			except Exception as er:
+				print("Failed to show entries")
+				print(">>>>>>>>>", er)
+		elif ch == 2:
+			try:
+				info = {}
+				info["ID"] = int(input("Department_ID: "))
+				info["Name"] = input("Department Name: ")
+				info["ENo"] = int(input("Number Of Employees: "))
+				query = "INSERT INTO Department(Department_ID, DepartmentName, NumberOfEmployees) VALUES('%d', '%s', '%d')" %(info["ID"], info["Name"], info["ENo"]);
+				cur.execute(query)
+				con.commit()
+				print("Successfully Added\n")
+			except Exception as er:
+				con.rollback()
+				print("Failed to insert into database")
+				print(">>>>>>>>>", er)
+		elif ch == 3:
+			try:
+				info = {}
+				ID = int(input("Enter Department_ID to modify: "))
+				query = "SELECT * FROM Department where Department_ID = '%d'" %(ID);
+				cur.execute(query)
+				row = cur.fetchone()
+				print("Enter the entries you want to modify otherwise press 'ENTER'")
+				info["ID"] = input("Department_ID: ")
+				if info["ID"] != "":
+                    row["Department_ID"] = int(info["ID"])
+				info["Name"] = input("Department Name: ")
+				if info["Name"] != "":
+                    row["DepartmentName"] = info["Name"]
+				info["ENo"] = input("Number Of Employees: ")
+				if info["ENo"] != "":
+                    row["NumberOfEmployees"] = int(info["ENo"])
+				query = "UPDATE Department SET Department_ID = '%d', DepartmentName = '%s', NumberOfEmployees = '%d' WHERE Department_ID = '%d'" %(row["Department_ID"], row["DepartmentName"], row["NumberOfEmployees"], ID);
+				cur.execute(query)
+				con.commit()
+				print("Successfully Modified\n")
+			except Exception as er:
+				con.rollback()
+				print("Failed to modify in the database")
+				print(">>>>>>>>>", er)
+		elif ch == 4:
+			try:
+				info = {}
+				info["ID"] = int(input("Enter the Department_ID you want to delete: "))
+				query = "DELETE FROM Department WHERE Department_ID = '%d'" %(info["ID"]);
+				cur.execute(query)
+				con.commit()
+				print("Successfully Deleted\n")
+			except Exception as er:
+				con.rollback()
+				print("Failed to delete in the database")
+				print(">>>>>>>>>", er)
+		elif ch == 5:
+			return 1
+		else:
+			print("Please choose the correct option")
+
+
+def employee(con, cur):
+	while(True):
+		print("Do you want to:")
+		print("1. See all entries")
+		print("2. Add new entry")
+		print("3. Modify the entry")
+		print("4. Delete the entry")
+		print("5. Go back")
+		ch = int(input("Enter choice> "))
+		if ch == 1:
+			try:
+				query = 'SELECT * FROM Employee;'
+				cur.execute(query)
+				rows = cur.fetchall()
+				for row in rows:
+					print(row)
+				print("")
+			except Exception as er:
+				print("Failed to show entries")
+				print(">>>>>>>>>", er)
+		elif ch == 2:
+			try:
+				info = {}
+				info["ID"] = int(input("Employee_ID: "))
+				info["Fname"], info["Mname"], info["Lname"] = (input("Name (Fname Minit Lname): ")).split(' ')	
+				info["DOB"] = input("DOB (YYYY-MM-DD): ")
+				info["Salary"] = int(input("Salary : "))
+				info["DID"] = int(input("Department_ID : "))
+				info["Gender"] = input("Gender (Male, Female, Others): ")
+				info["Address"] = input("Address : ")
+				info["Pin Code"] = int(input("Pin Code : "))
+				info["State"] = input("State : ")
+				query = "INSERT INTO Employee(Employee_ID, First_Name, Middle_Name, Last_Name, DOB, Gender, Address, PinCode, State, Salary, Department_ID) VALUES('%d', '%s', '%s', '%s', '%s', '%s', '%s', '%d', '%s', '%d', '%d')" %(info["ID"], info["Fname"], info["Mname"], info["Lname"], info["DOB"], info["Gender"], info["Address"], info["Pin Code"], info["State"], info["Salary"], info["DID"]);
+				cur.execute(query)
+				con.commit()
+				print("Successfully Added\n")
+			except Exception as er:
+				con.rollback()
+				print("Failed to insert into database")
+				print(">>>>>>>>>", er)
+		elif ch == 3:
+			try:
+				info = {}
+				ID = int(input("Enter Employee_ID to modify: "))
+				query = "SELECT * FROM Employee where Employee_ID = '%d'" %(ID);
+				cur.execute(query)
+				row = cur.fetchone()
+				print("Enter the entries you want to modify otherwise press 'ENTER'")
+				info["ID"] = input("Employee_ID: ")
+				if info["ID"] != "":
+					row["Employee_ID"] = int(info["ID"])
+				info["name"] = input("Name (Fname Minit Lname): ")
+				if info["name"] != "":
+					row["First_Name"], row["Middle_Name"], row["Last_Name"] = info["name"].split(' ')	
+				info["DOB"] = input("DOB (YYYY-MM-DD): ")
+				if info["DOB"] != "":
+					row["DOB"] = info["DOB"]
+				info["Gender"] = input("Gender (Male, Female, Others): ")
+				if info["Gender"] != "":
+					row["Gender"] = info["Gender"]
+				info["Address"] = input("Address : ")
+				if info["Address"] != "":
+					row["Address"] = info["Address"]
+				info["Pin Code"] = input("Pin Code : ")
+				if info["Pin Code"] != "":
+					row["PinCode"] = int(info["Pin Code"])
+				info["State"] = input("State : ")
+				if info["State"] != "":
+					row["State"] = info["State"]
+				info["Salary"] = input("Salary: ")
+				if info["Salary"] != "":
+					row["Salary"] = int(info["Salary"])
+				info["DID"] = input("Department_ID: ")
+				if info["DID"] != "":
+					row["Department_ID"] = int(info["DID"])
+				query = "UPDATE Employee SET Employee_ID = '%d', First_Name = '%s', Middle_Name = '%s', Last_Name = '%s', DOB = '%s', Gender = '%s', Address = '%s', PinCode = '%d', State = '%s', Salary = '%d', Department_ID = '%d' WHERE Employee_ID = '%d'" %(row["Employee_ID"], row["First_Name"], row["Middle_Name"], row["Last_Name"], row["DOB"], row["Gender"], row["Address"], row["PinCode"], row["State"], row["Salary"], row["Department_ID"], ID);
+				cur.execute(query)
+				con.commit()
+				print("Successfully Modified\n")
+			except Exception as er:
+				con.rollback()
+				print("Failed to modify in the database")
+				print(">>>>>>>>>", er)
+		elif ch == 4:
+			try:
+				info = {}
+				info["ID"] = int(input("Enter the Employee_ID you want to delete: "))
+				query = "DELETE FROM Employee WHERE Employee_ID = '%d'" %(info["ID"]);
+				cur.execute(query)
+				con.commit()
+				print("Successfully Deleted\n")
+			except Exception as er:
+				con.rollback()
+				print("Failed to delete in the database")
+				print(">>>>>>>>>", er)
+		elif ch == 5:
+			return 1
+		else:
+			print("Please choose the correct option")
+
--- ship_management_system.py
+++ ship_management_system.py
@@ -3,82 +3,6 @@ import pymysql
 import pymysql.cursors
 from adminFunctions import *
 
-def passengerShip(con, cur):
-	while(True):
-		print("Do you want to:")
-		print("1. See all entries")
-		print("2. Add new entry")
-		print("3. Modify the entry")
-		print("4. Delete the entry")
-		print("5. Go back")
-		ch = int(input("Enter choice> "))
-		if ch == 1:
-			try:
-				query = 'SELECT * FROM PassengerShip;'
-				cur.execute(query)
-				rows = cur.fetchall()
-				for row in rows:
-					print(row)
-				print("")
-			except Exception as er:
-				print("Failed to show entries")
-				print(">>>>>>>>>", er)
-		elif ch == 2:
-			try:
-				info = {}
-				info["ID"] = int(input("Ship_ID: "))
-				info["Capacity"] = float(input("Capacity (TEU) : "))
-				info["Facilities"] = input("Facilities : ")
-				query = "INSERT INTO PassengerShip(Ship_ID, Capacity(TEU), Facilities) VALUES('%d', '%f', '%s')" %(info["ID"], info["Capacity"], info["Facilities"]);
-				cur.execute(query)
-				con.commit()
-				print("Successfully Added\n")
-			except Exception as er:
-				con.rollback()
-				print("Failed to insert into database")
-				print(">>>>>>>>>", er)
-		elif ch == 3:
-			try:
-				info = {}
-				ID = int(input("Enter Ship_ID to modify: "))
-				query = "SELECT * FROM PassengerShip where Ship_ID = '%d'" %(ID);
-				cur.execute(query)
-				row = cur.fetchone()
-				print("Enter the entries you want to modify otherwise press 'ENTER'")
-				info["ID"] = input("Ship_ID: ")
-				if info["ID"] != "":
-					row["Ship_ID"] = int(info["ID"])
-				info["Capacity"] = input("Capacity (TEU) : ")	
-				if info["Capacity"] != "":
-					row["Capacity(TEU)"] = float(info["Capacity"])
-				info["Facilities"] = input("Facilities : ")
-				if info["Facilities"] != "":
-					row["Facilities"] = info["Facilities"]
-				query = "UPDATE PassengerShip SET Ship_ID = '%d', Capacity(TEU) = '%f', Facilities = '%s' WHERE Ship_ID = '%d'" %(row["Ship_ID"], row["Capacity(TEU)"], row["Facilities"], ID);
-				cur.execute(query)
-				con.commit()
-				print("Successfully Modified\n")
-			except Exception as er:
-				con.rollback()
-				print("Failed to modify in the database")
-				print(">>>>>>>>>", er)
-		elif ch == 4:
-			try:
-				info = {}
-				info["ID"] = int(input("Enter the Ship_ID you want to delete: "))
-				query = "DELETE FROM PassengerShip WHERE Ship_ID = '%d'" %(info["ID"]);
-				cur.execute(query)
-				con.commit()
-				print("Successfully Deleted\n")
-			except Exception as er:
-				con.rollback()
-				print("Failed to delete in the database")
-				print(">>>>>>>>>", er)
-		elif ch == 5:
-			return 1
-		else:
-			print("Please choose the correct option")
-
 
 def admin(con, cur):
 	while(True):
@@ -116,16 +40,16 @@ def admin(con, cur):
 			cargoShip(con, cur)
 		elif ch == 5:
 			passengerShip(con, cur)
-		# elif ch == 6:
-		# 	agent(con, cur)
-		# elif ch == 7:
-		# 	route(con, cur)
-		# elif ch == 8:
-		# 	ticket(con, cur)
-		# elif ch == 9:
-		# 	department(con, cur)
-		# elif ch == 10:
-		# 	employee(con, cur)
+		elif ch == 6:
+			agent(con, cur)
+		elif ch == 7:
+			route(con, cur)
+		elif ch == 8:
+			ticket(con, cur)
+		elif ch == 9:
+			department(con, cur)
+		elif ch == 10:
+			employee(con, cur)
 		# elif ch == 11:
 		# 	security(con, cur)
 		# elif ch == 12:
@@ -153,7 +77,7 @@ def admin(con, cur):
 		else:
 			print("Please choose the correct option")
 
-def user(con, cur):
+def user(id, con, cur):
 	while(True):
 		print("Do you want to:")
 		print("1. Book Ticket")
@@ -163,49 +87,61 @@ def user(con, cur):
 			return 1
 		elif ch == 2:
 			return 1
-			
+		else:
+			print("Please choose the correct option")
 
 def login(con, cur):
 	try:
-		return 1
-	except:
-		return 0
+		info = {}
+		info["ID"] = int(input("Passenger_ID: "))
+		query = "SELECT * FROM Passenger WHERE Passenger_ID = '%d'" %(info["ID"]);
+		cur.execute()
+		row = cur.fetchone()
+		if row == None:
+			print("You are not registered.")
+			return
+		user(info["ID"], con, cur)
+	except Exception as er:
+		print(er)
 
 def register(con, cur):
 	try:
 		info = {}
 		info["ID"] = int(input("Passenger_ID: "))
+		query = "SELECT * FROM Passenger WHERE Passenger_ID = '%d'" %(info["ID"]);
+		cur.execute()
+		row = cur.fetchone()
+		if row != None:
+			print("Passenger_ID already exists.")
+			return
 		info["Fname"], info["Mname"], info["Lname"] = (input("Name (Fname Minit Lname): ")).split(' ')	
 		info["DOB"] = input("DOB (YYYY-MM-DD): ")
 		info["Gender"] = input("Gender (Male, Female, Others): ")
 		info["Address"] = input("Address : ")
 		info["Pin Code"] = int(input("Pin Code : "))
 		info["State"] = input("State : ")
-
 		query = "INSERT INTO Passenger(Passenger_ID, First_Name, Middle_Name, Last_Name, DOB, Gender, Address, PinCode, State) VALUES('%d', '%s', '%s', '%s', '%s', '%s', '%s', '%d', '%s')" %(info["ID"], info["Fname"], info["Mname"], info["Lname"], info["DOB"], info["Gender"], info["Address"], info["Pin Code"], info["State"]);
 		cur.execute(query)
 		con.commit()
 		print("Successfully Registered\n")
-		return 1
-
+		user(info["ID"], con, cur)
 	except Exception as er:
 		con.rollback()
 		print("Failed to insert into database")
 		print(">>>>>>>>>", er)
-		return 0
 
 
 if __name__ == '__main__':
 	run = 1
 	while(run):
 		tmp = sp.call('clear',shell=True)
-		# username = input("Username: ")
-		# password = input("Password: ")
+		username = input("Username: ")
+		password = input("Password: ")
 		
 		try:
 			con = pymysql.connect(host='localhost',
-					user='user',
-					password='password',
+					user=username,
+					password=password,
 					db='SHIP_MANAGEMENT_SYSTEM',
 					cursorclass=pymysql.cursors.DictCursor)
 			tmp = sp.call('clear',shell=True)
@@ -228,15 +164,11 @@ if __name__ == '__main__':
 					print("4. Exit the system")
 					ch = int(input("Enter choice> "))
 					if ch == 1:
-						if register(con, cur):
-							user(con, cur)
-						else:
-							tmp = input("Press ENTER to CONTINUE>")
+						register(con, cur)
+						tmp = input("Press ENTER to CONTINUE>")
 					elif ch == 2:
-						if login(con, cur):
-							user(con, cur)
-						else:
-							tmp = input("Press ENTER to CONTINUE>")
+						login(con, cur)
+						tmp = input("Press ENTER to CONTINUE>")
 					elif ch == 3:
 						while(True):
 							tmp = input("Enter the password to CONTINUE> ")
